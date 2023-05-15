376b778554c9a27fb9318f9791ff68a44c8edcca
Joshua Oluwakuse <kusejoshua@gmail.com>
2023-05-16 01:45:54 +0200

    committed

--- src/main/java/com/example/crossfundwallet/Data/Models/User.java
+++ src/main/java/com/example/crossfundwallet/Data/Models/User.java
@@ -1,2 +1,25 @@
-package com.example.crossfundwallet.Data.Models;public class User {
+package com.example.crossfundwallet.Data.Models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String EmailAddress;
+    private LocalDate DOB;
+    private LocalDate DateOfRegistration;
+    private List<Account> account = new ArrayList<>();
+    private int age;
+    private String password;
+    private List<Card> cards = new ArrayList<>();
+
+
 }
