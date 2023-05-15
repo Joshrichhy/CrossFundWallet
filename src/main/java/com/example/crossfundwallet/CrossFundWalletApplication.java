3e1631216f0f7662a03b2a909b4469cb04265cfc
Joshua Oluwakuse <kusejoshua@gmail.com>
2023-05-16 00:53:41 +0200

    first commit

---
+++ src/main/java/com/example/crossfundwallet/CrossFundWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.crossfundwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossFundWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrossFundWalletApplication.class, args);
+    }
+
+}
